b197a196a7d2d54d575cd30a51de28b3ad165106
uankuo34-lang <uankuo34@gmail.com>
2025-11-29 20:06:14 +0800

    步进电机解锁成功

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -1,31 +1,43 @@
-#include <lib/lib/lib.h>
-#include <lib/WiFi/WiFiCommunication.h>
+#include <Arduino.h>
+
+#define I2S_OUT_BCK             16      //正确的引脚
+#define I2S_OUT_WS              17
+#define I2S_OUT_DATA            21
+u_int8_t LOCK = 0b10000000;
+u_int8_t direction = 0b10000010;
+u_int8_t PINS_X = 0b10000001;
+
+u_int8_t VOID = 0b00000000;
+
+
+
 
 void setup(){
-    Serial.begin(115200);
-    Serial.println("程序已起动");
-    WiFiCommunication WiFiCommunication("CMCC-Wm2n", "ghzbv5g8");
+    pinMode(I2S_OUT_BCK, OUTPUT);
+    pinMode(I2S_OUT_WS, OUTPUT);
+    pinMode(I2S_OUT_DATA, OUTPUT);
 
-    while(true){
-      StringList text = WiFiCommunication.read();
+    digitalWrite(I2S_OUT_WS, LOW);
+    shiftOut(I2S_OUT_DATA, I2S_OUT_BCK, MSBFIRST, LOCK);
+    digitalWrite(I2S_OUT_WS, HIGH);
+    delayMicroseconds(100);
 
-      if(text.size() > 0){
-        for(int i=text.size()-1; i >= 0; i--){
+    digitalWrite(I2S_OUT_WS, LOW);
+    shiftOut(I2S_OUT_DATA, I2S_OUT_BCK, MSBFIRST, direction);
+    digitalWrite(I2S_OUT_WS, HIGH);
+    delayMicroseconds(100);
 
-        Serial.println(text[i]);
-        Serial.print("  ");
 
-        if(text[i] == "exit()"){
-          WiFiCommunication.exit();
-          return;
-        }
-      }
-      Serial.println("");
-      vTaskDelay(250);
-      }
-    }
 }
+
 void loop(){
-  //TODO
-}
+        digitalWrite(I2S_OUT_WS, LOW);
+        shiftOut(I2S_OUT_DATA, I2S_OUT_BCK, MSBFIRST, PINS_X);
+        digitalWrite(I2S_OUT_WS, HIGH);
+        delayMicroseconds(100);
 
+        digitalWrite(I2S_OUT_WS, LOW);
+        shiftOut(I2S_OUT_DATA, I2S_OUT_BCK, MSBFIRST, VOID);
+        digitalWrite(I2S_OUT_WS, HIGH);
+        delayMicroseconds(100);
+}
